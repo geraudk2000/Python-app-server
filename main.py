e1d5861c252ab32392b3a0a738f554b702a5ec83
Geraud <geraudkameni@gmail.com>
2023-05-20 23:51:21 +0200

    modified  main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 HOST = '0.0.0.0'
 PORT = 8080
 
+
 class Handler(BaseHTTPRequestHandler):
-    
 
     def do_GET(self):
-       self.send_response(200)
-       self.send_header('Content-Type', 'text/plain')
-       self.end_headers()
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
 
-       message = 'Hello World!' 
-       self.wfile.write(bytes(message, 'utf-8'))
+        message = 'Hello World!'
+        self.wfile.write(bytes(message, 'utf-8'))
+
 
 def run(host, port, server_class=HTTPServer, handler_class=Handler):
-    
     server_address = (host, port)
     httpd = server_class(server_address, handler_class)
 
@@ -25,6 +25,7 @@ def run(host, port, server_class=HTTPServer, handler_class=Handler):
         pass
 
     httpd.server_close()
+
 
 if __name__ == '__main__':
     run(HOST, PORT)
