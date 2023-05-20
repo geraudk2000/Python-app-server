035d4d98d87f6f078bb669e3695ef1f1438d1a93
Geraud <geraudkameni@gmail.com>
2023-05-20 23:46:46 +0200

    modified  main.py

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ class Handler(BaseHTTPRequestHandler):
        self.end_headers()
 
        message = 'Hello World!' 
+       self.wfile.write(bytes(message, 'utf-8'))
+
+def run(host, port, server_class=HTTPServer, handler_class=Handler):
+    
+    server_address = (host, port)
+    httpd = server_class(server_address, handler_class)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    httpd.server_close()
+
+if __name__ == '__main__':
+    run(HOST, PORT)
